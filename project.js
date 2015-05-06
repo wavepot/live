d482b2f7d0003c07f0d41afb2d2b8ffc5a922dc4
George Stagas <gstagas@gmail.com>
2015-05-06 12:57:38 +0300

    add project metadata

---
+++ project.js
@@ -0,0 +1,14 @@
+(function() {
+
+// register
+
+var project = self.project = {};
+
+// properties
+
+project.name = 'wavepot/live';
+project.source = 'https://github.com/wavepot/live';
+project.version = '0.0.1';
+project.license = 'MIT';
+
+})();
